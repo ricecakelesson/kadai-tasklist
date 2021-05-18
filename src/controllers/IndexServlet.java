96461d540637e5aa242eae5b75956547de4ca580
ricecake.lesson <ricecake.lesson@gmail.com>
2021-05-18 11:38:35 +0900

    END tasklist

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -28,7 +34,43 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (NumberFormatException e) {
+        }
+
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                .setFirstResult(5 * (page - 1))
+                .setMaxResults(5)
+                .getResultList();
+
+        // 全件数を取得
+        long tasks_count = (long) em.createNamedQuery("getTasksCount", Long.class)
+                .getSingleResult();
+
+        em.close();
+
+
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        if(request.getSession().getAttribute("flush") != null) {
+            // セッションスコープ内のフラッシュメッセージをリクエストスコープに保存し、セッションスコープからは削除する
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count); // 全件数
+        request.setAttribute("page", page); // ページ数
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
+
+
     }
 
 }
